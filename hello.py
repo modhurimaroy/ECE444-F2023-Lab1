a99053f0b0530490949aa92cd559c7e956b2c70c
Modhurima Roy Kenopy <modhurima@Modhurimas-MacBook-Air.local>
2023-09-24 16:22:16 -0400

    Completed the second step of activity 4, this marks the end of activity 4

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -14,19 +14,30 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is you UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+    
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
+        # incorrect = "False"
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        
+        session['email'] = form.email.data
         session['name'] = form.name.data
+        
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    
+    return render_template('index.html', form=form, name=session.get('name'), 
+                           email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
